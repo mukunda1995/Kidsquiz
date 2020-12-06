4f8143a2651bcebf95025af4a69347dcb8fdde4b
mukunda <evilmukunda@gmail.com>
2020-12-06 17:54:34 +0800

    kidsquiz

---
+++ app/src/main/java/com/example/kidsquiz/Database.java
@@ -0,0 +1,25 @@
+package com.example.kidsquiz;
+
+public class Database {
+
+    Integer[] animals ={
+            R.drawable.cat,
+            R.drawable.dog,
+            R.drawable.horse,
+            R.drawable.chicken,
+            R.drawable.lion,
+            R.drawable.owl,
+            R.drawable.tortise
+
+    };
+
+    String[] answers = {
+            "cat",
+            "dog",
+            "horse",
+            "chicken",
+            "lion",
+            "owl",
+            "turtle"
+    };
+}
---
+++ app/src/main/java/com/example/kidsquiz/ItemAnimal.java
@@ -0,0 +1,19 @@
+package com.example.kidsquiz;
+
+public class ItemAnimal {
+    String name;
+    int image;
+
+    public ItemAnimal(String name,int image) {
+        this .name = name;
+        this .image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
